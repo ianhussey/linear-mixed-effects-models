9d402ae678e97cfe3da03f79ab02093fd689deb9
ianhussey <Ian@g021h123.guest.ugent.be>
2016-11-21 15:09:29 +0100

    tidied

--- data processing.r
+++ data processing.r
@@ -1,5 +1,10 @@
-# Title: Process data from derivation experiment 2
-# Author: Ian Hussey (ian.hussey@ugent.be)
+# title: process IAT data
+# author: Ian Hussey (ian.hussey@ugent.be)
+# license: 
+#   code: GPLv3+        https://www.gnu.org/licenses/quick-guide-gplv3.en.html
+#   data: CC BY 4.0     https://creativecommons.org/licenses/by/4.0/
+#         please cite:  Hussey, I., & Hughes, S. (2016). Transitive relations and implicit attitudes. Dataset. osf.io/5nxby 
+
 
 # Clean the workspace
 rm(list=ls())
@@ -7,12 +12,14 @@ rm(list=ls())
 
 # Dependencies ------------------------------------------------------------
 
+
 library(plyr)
 library(tidyverse)
 library(data.table)
 
 
 # Data acquisition and cleaning -------------------------------------------
+
 
 ## Set the working directory
 setwd("/Users/Ian/Dropbox/Work/Projects/Alphabet soup/Hussey & Hughes - Derivation study/OSF - Transitive relations and implicit attitudes/Experiment 3/Raw Data")
@@ -48,17 +55,31 @@ cleaned_df <-
                 trial_n = as.numeric(trial_n),
                 accuracy = as.numeric(accuracy),
                 rt = as.numeric(rt),
-                condition = ifelse(participant%%2 == 1, 1, ifelse(participant%%2 == 0, 2, NA)))
+                condition = ifelse(participant %% 2 == 1, 1, ifelse(participant %% 2 == 0, 2, NA)))  # condition was assigned based on odd/even participant code
 
 # MANUAL INCLUSIONS HERE - ONLY THOSE WITH COMPLETE DATA
 inclusion_df <- read.csv("/Users/Ian/Dropbox/Work/Projects/Alphabet soup/Hussey & Hughes - Derivation study/OSF - Transitive relations and implicit attitudes/Experiment 3/Processed data/inclusion list.csv")
-
 cleaned_df <- dplyr::inner_join(cleaned_df, inclusion_df, by = "participant")
 
-# IAT rts -----------------------------------------------------------------
+
+# subsample data ----------------------------------------------------------
 
 
-# select relevant data
+# full dataset is not needed to demo the analyses
+
+subsample_participants <- 
+  cleaned_df %>% 
+  distinct(participant) %>% 
+  slice(1:50)  # first 50 participants
+
+cleaned_df <- 
+  cleaned_df %>% 
+  inner_join(subsample_participants, by = "participant")  # include only participant codes from the above slice
+
+
+# extract IAT rts ---------------------------------------------------------
+
+
 IAT_data <-  
   cleaned_df %>%
   dplyr::filter(block_name == "IAT_test_compatible_block" | block_name == "IAT_test_incompatible_block") %>%
@@ -73,5 +94,6 @@ IAT_data <-
 
 # Write to disk -----------------------------------------------------------
 
-write.csv(IAT_data, file = "/Users/Ian/git/linear mixed effects models/long data.csv", row.names = FALSE)
+
+IAT_data %>% write.csv(file = "/Users/Ian/git/linear mixed effects models/example data.csv", row.names = FALSE)
 
--- mixed effects models.R
+++ mixed effects models.R
@@ -1,6 +1,10 @@
-# title: mixed linear effects models for RT data
+# title: mixed effects models - linear frequentist, linear bayes factors, and semi-parametric Probabalistic Index
 # author: Ian Hussey (ian.hussey@ugent.be)
-# license: GPLv3+
+# license: 
+#   code: GPLv3+        https://www.gnu.org/licenses/quick-guide-gplv3.en.html
+#   data: CC BY 4.0     https://creativecommons.org/licenses/by/4.0/
+#         please cite:  Hussey, I., & Hughes, S. (2016). Transitive relations and implicit attitudes. Dataset. osf.io/5nxby 
+
 
 # dependencies ------------------------------------------------------------
 
@@ -21,7 +25,7 @@ library(pim)
 setwd("/Users/Ian/git/linear mixed effects models/")
 
 IAT_data <- 
-  read.csv("long data.csv") %>%
+  read.csv("example data.csv") %>%
   filter(rt > 0) %>%  # log transformations require rts of 0 to be removed.
   mutate(block = ifelse(block == 1, "B1C1_block", "B1C2_block"),
          condition = as.factor(ifelse(condition == 1, "IAT_first", "IAT_second")),
@@ -96,16 +100,17 @@ model_1 <- afex::mixed(log_rt ~ block * condition + (1 | participant), # enterin
                        progress = TRUE, 
                        return = "mixed")
 
+save(model_1, file = "model_1_lmm_freq.RData")
+#load(model_1, file = "model_1_lmm_freq.RData")
+
 summary(model_1)
 print(model_1)  # same as using anova() here
-
 
 # write to disk
 sink("1 frequentist linear mixed effects model - log transformation.txt")
 summary(model_1)
 print(model_1)  # same as using anova() here
 sink()
-
 
 
 # power analysis for mixed linear effects models ---------------------------
@@ -135,13 +140,15 @@ model_2 <- generalTestBF(log_rt ~ block * condition + participant,
                          rscaleRandom = "nuisance",  # default
                          multicore = TRUE) 
 
+save(model_2, file = "model_2_lmm_BF.RData")
+#load(model_2, file = "model_2_lmm_BF.RData")
+
 # all BF models
 model_2
 
 # BFs are transitive, so the contribution of the interaction is calculated by
 # dividing the full model by the model without the interaction.
 model_2[9] / model_2[8]
-
 
 # write to disk
 sink("2 BF linear mixed effects model - log transformation.txt")
@@ -155,20 +162,36 @@ sink()
 # model 3 -----------------------------------------------------------------
 
 
-# semi parametric effects model
+# semi parametric effects model - RUN TIME is c.3 HOURS!
+# needs random effect - not currently supported by PIM library.
+# uncompressed model is 1.6GB
 model_3 <- pim(rt ~ block * condition,
                data = IAT_data)
 
-save(model_3, file = "model_3_pim.RData")
+#save(model_3, file = "model_3_pim.RData")
 #load(model_3, file = "model_3_pim.RData")
 
-
+# summary
 summary(model_3)
 
 # p value to exact decimal places
-summary(pr(model_3)
+summary(model_3)[,4]
 
-#effect size
+# effect size
 plogis(coef(model_3))
+
+# 95% CIs on the PI effect size.
+plogis(confint(model_3))
+
+# write to disk
+sink("3 semi-parametric fixed effects model.txt")
+cat("SUMMARY \n\n")
+summary(model_3)
+cat("\n\nEXACT P VALUES \n\n")
+summary(model_3)[,4]
+cat("\n\nEFFECT SIZES \n\n")
+plogis(coef(model_3))
+cat("\n\n95% CIs ON THE ES \n\n")
+sink()
 
 
